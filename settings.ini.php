846fb911965e953240bfef8636b48450bdbab9f0
Patrick Emond <emondpd@mcmaster.ca>
2024-12-20 10:38:07 -0500

    Tweak: updating build version for last commit

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.10';
-$settings['general']['cenozo_build'] = 'a8f1cb51';
+$settings['general']['cenozo_build'] = 'ee7d5ad7';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
