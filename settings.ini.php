96211a4c6b5e35346003d924c073d85bc0214df5
Patrick Emond <emondpd@mcmaster.ca>
2025-01-07 13:22:17 -0500

    Libs: upgrading new NPM modules

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.10';
-$settings['general']['cenozo_build'] = 'ee7d5ad7';
+$settings['general']['cenozo_build'] = '1d730ceb';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
